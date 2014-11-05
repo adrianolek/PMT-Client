c6e31e0c0f26ce8a0c24863c6306063eafb407cd
Adrian Olek <adrianolek@gmail.com>
2014-11-05 21:49:51 +0100

    Added login resource

--- app/login/login.js
+++ app/login/login.js
@@ -1,19 +1,41 @@
 'use strict';
 
-angular.module('pmtClient.login', ['ngRoute'])
+angular.module('pmtClient.login', ['ngRoute', 'ngResource'])
 
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/login', {templateUrl: 'login/login.html', controller: 'LoginCtrl'});
   }])
 
-  .controller('LoginCtrl', ['$scope', function ($scope) {
-    $scope.login = function () {
-      if (!$scope.login.url || !$scope.login.url.match(/^https?:\/\/.+/)) {
+  .factory('LoginResource', ['$resource',
+    function ($resource) {
+      return $resource('/api/token.json', {}, {
+        query: {method: 'POST'}
+      });
+    }])
+
+  .controller('LoginCtrl', ['$scope', 'LoginResource', function ($scope, LoginResource) {
+    $scope.url = '';
+    $scope.login = {
+      username: '',
+      password: ''
+    };
+
+    $scope.doLogin = function () {
+      if (!$scope.url.match(/^https?:\/\/.+/)) {
         $scope.info = '';
         $scope.error = 'Url is invalid';
       } else {
         $scope.info = 'Logging in...';
         $scope.error = '';
+
+        LoginResource.query($scope.login, function (user) {
+          $scope.info = '';
+          if (user.token) {
+            $scope.error = '';
+          } else {
+            $scope.error = 'Invalid username or password.'
+          }
+        });
       }
     }
   }]);
