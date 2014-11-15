f2c68b7e9fd3c03ba394a0721ab11314e50ea916
Adrian Olek <adrianolek@gmail.com>
2014-11-15 17:39:17 +0100

    Added estimation page

--- app/tasks/tasks.js
+++ app/tasks/tasks.js
@@ -5,6 +5,7 @@ angular.module('pmtClient.tasks', ['ngRoute', 'ngResource'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/project/:projectId/tasks', {templateUrl: 'tasks/index.html', controller: 'IndexCtrl'});
     $routeProvider.when('/project/:projectId/task/:taskId', {templateUrl: 'tasks/show.html', controller: 'ShowCtrl'});
+    $routeProvider.when('/project/:projectId/task/:taskId/estimate', {templateUrl: 'tasks/estimate.html', controller: 'EstimateCtrl'});
   }])
 
   .controller('IndexCtrl', ['$scope', 'ApiClient', '$routeParams', function ($scope, ApiClient, $routeParams) {
@@ -17,11 +18,30 @@ angular.module('pmtClient.tasks', ['ngRoute', 'ngResource'])
   .controller('ShowCtrl', ['$scope', 'ApiClient', '$routeParams', '$location',
     function ($scope, ApiClient, $routeParams, $location) {
     ApiClient.tasks().get($routeParams, function (res) {
+      if (res.task.estimatedTime == 0) {
+        $location.path('project/' + $routeParams.projectId + '/task/' + $routeParams.taskId + '/estimate');
+        return;
+      }
+
       $scope.project = res.project;
       $scope.task = res.task;
     });
 
     $scope.finish = function () {
       $location.path('project/' + $scope.project.id + '/tasks');
+    };
+  }])
+  .controller('EstimateCtrl', ['$scope', '$routeParams', '$location', 'ApiClient',
+    function ($scope, $routeParams, $location, ApiClient) {
+    ApiClient.tasks().get($routeParams, function (res) {
+      $scope.project = res.project;
+      $scope.task = res.task;
+      $scope.estimate = res.task.estimatedTime;
+    });
+
+    $scope.save = function () {
+      if ($scope.estimate > 0) {
+        $location.path('project/' + $routeParams.projectId + '/task/' + $routeParams.taskId);
+      }
     };
   }]);
