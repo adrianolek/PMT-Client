17e00d6cf0611c0dbfdbcaf348962e65f5b8f347
Adrian Olek <adrianolek@gmail.com>
2015-01-03 18:41:50 +0100

    Added build task

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,12 @@ var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 var minifyHTML = require('gulp-minify-html');
+var runSequence = require('run-sequence');
 
 gulp.task('copy', function () {
-  gulp.src(['**', '!bower_components/**'], {cwd: 'app', nodir: true})
-    .pipe(gulp.dest('build/app'));
-
-  gulp.src(['.bowerrc', 'background.js', 'bower.json', 'icon-16.png', 'icon-128.png', 'manifest.json'])
+  return gulp.src(['app/**', '!app/bower_components/**',
+    '.bowerrc', 'background.js', 'bower.json',
+    'icon-16.png', 'icon-128.png', 'manifest.json'], {nodir: true, base: '.'})
     .pipe(gulp.dest('build'));
 });
 
@@ -21,7 +21,7 @@ gulp.task('bower', function() {
 });
 
 gulp.task('clean', function () {
-  del(['build', 'dist']);
+  return del(['build', 'dist']);
 });
 
 gulp.task('zip', function () {
@@ -30,37 +30,47 @@ gulp.task('zip', function () {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('assets', function () {
+gulp.task('assets-useref', function(){
   var assets = useref.assets();
 
-  gulp.src('build/app/index.html')
+  return gulp.src('build/app/index.html')
     .pipe(assets)
     .pipe(gulpif('*.js', uglify()))
     .pipe(gulpif('*.css', minifyCss({keepSpecialComments: 0})))
     .pipe(assets.restore())
     .pipe(useref())
     .pipe(gulp.dest('build/app'));
+});
 
-  gulp.src('build/background.js')
+gulp.task('assets-uglify', function(){
+  return gulp.src('build/background.js')
     .pipe(uglify())
     .pipe(gulp.dest('build'));
+});
 
-  gulp.src('build/app/bower_components/bootstrap/fonts/*')
+gulp.task('assets-fonts', function(){
+  return gulp.src('build/app/bower_components/bootstrap/fonts/*')
     .pipe(gulp.dest('build/app/fonts'));
 });
 
+gulp.task('assets', ['assets-useref', 'assets-uglify', 'assets-fonts']);
+
 gulp.task('minify-html', function() {
-  gulp.src('build/app/**/*.html')
+  return gulp.src(['build/app/**/*.html', '!build/app/bower_components/**'])
     .pipe(minifyHTML({empty: true}))
     .pipe(gulp.dest('build/app'))
 });
 
 gulp.task('cleanup', function () {
-  del(['build/app/bower_components',
+  return del(['build/app/bower_components',
     'build/app/css/app.css',
     'build/.bowerrc',
     'build/bower.json',
     'build/app/**/*.js',
     'build/app/services',
     '!build/app/js/all.js']);
+});
+
+gulp.task('build', function() {
+  runSequence('clean', 'copy', 'bower', 'assets', ['minify-html', 'cleanup']);
 });
