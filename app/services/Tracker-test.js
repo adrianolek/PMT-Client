11e18dfcea7149303978440e47c90b64ee2b5369
Adrian Olek <adrianolek@gmail.com>
2015-01-20 22:28:03 +0100

    Added ApiClient mock

--- app/services/Tracker-test.js
+++ app/services/Tracker-test.js
@@ -2,4 +2,10 @@
 
 describe('Tracker', function() {
   beforeEach(module('pmtClient.tracker'));
+
+  beforeEach(function () {
+    module('pmtClient.api', function ($provide) {
+      $provide.value('ApiClient', {});
+    });
+  });
 });
