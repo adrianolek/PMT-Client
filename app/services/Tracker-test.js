9caf51c8aec6d14fa286917dc7d60fb1970ce5b4
Adrian Olek <adrianolek@gmail.com>
2015-01-22 21:06:24 +0100

    Added tracker description test

--- app/services/Tracker-test.js
+++ app/services/Tracker-test.js
@@ -12,4 +12,9 @@ describe('Tracker', function() {
   it('time should be negative',  inject(function(Tracker) {
     expect(Tracker.getTime()).toBe(-10);
   }));
+
+  it('description should be set',  inject(function(Tracker) {
+    Tracker.setDescription('foo');
+    expect(Tracker.description).toBe('foo');
+  }));
 });
