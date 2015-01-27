76792fe04ab640a7d0c00d2f935c8f2715777740
Adrian Olek <adrianolek@gmail.com>
2015-01-27 17:46:32 +0100

    Load sprintf for tests

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function(config) {
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/bower_components/angular-resource/angular-resource.js',
+      'app/bower_components/sprintf/dist/sprintf.min.js',
       'app/+(login|projects|services|tasks)/*.js'
     ],
     exclude: [],
