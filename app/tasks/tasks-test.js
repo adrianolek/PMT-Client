7d1122be0cb0043aafa62c88f8005d4a0c8c576d
Adrian Olek <adrianolek@gmail.com>
2015-02-18 17:59:21 +0100

    Added tasks load test

--- app/tasks/tasks-test.js
+++ app/tasks/tasks-test.js
@@ -6,9 +6,19 @@ describe('IndexCtrl', function () {
   beforeEach(module('pmtClient.tasks'));
 
   beforeEach(function () {
-    ApiClient = {};
+    ApiClient = {
+      tasks: function() {
+        return {
+          query: function(params, cb) {
+            cb({project: 'foo', tasks: 'bar'})
+          }
+        };
+      }
+    };
 
-    Tracker = {};
+    Tracker = {
+      idle: function() {}
+    };
 
     module(function ($provide) {
       $provide.value('ApiClient', ApiClient);
@@ -21,4 +31,9 @@ describe('IndexCtrl', function () {
     $scope = $rootScope.$new();
     $controller = _$controller_('IndexCtrl', {$scope: $scope, ApiClient: ApiClient, Tracker: Tracker});
   }));
+
+  it('should load tasks', function(){
+    expect($scope.project).toBe('foo');
+    expect($scope.tasks).toBe('bar');
+  });
 });
