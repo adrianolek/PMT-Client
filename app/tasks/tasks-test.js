c7750cb3999497707bf9d5fcaf2da48605456c24
Adrian Olek <adrianolek@gmail.com>
2015-02-21 22:45:27 +0100

    Added estimate redirect test

--- app/tasks/tasks-test.js
+++ app/tasks/tasks-test.js
@@ -78,4 +78,24 @@ describe('ShowCtrl', function () {
     expect($scope.project).toBe('foo');
     expect($scope.task.name).toBe('bar');
   });
+
+  it('should redirect to estimate', inject(function($rootScope, _$controller_){
+    $scope = $rootScope.$new();
+    ApiClient.tasks = function() {
+      return {
+        get: function(params, cb) {
+          cb({project: 'foo',
+            task: {
+              estimatedTime: 0
+            }});
+        }
+      };
+    };
+    var location = {
+      path: function(p){
+        expect(p).toMatch(/estimate$/);
+      }
+    };
+    $controller = _$controller_('ShowCtrl', {$scope: $scope, ApiClient: ApiClient, Tracker: Tracker, $location: location});
+  }));
 });
